3312727a6a7a77c61cf46d2ce7b9f668ee612b35
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-07-29 01:36:00 +0530

    Bug fixes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
    res.sendFile(path.join(_dirname, 'build', 'index.html'));
 });
